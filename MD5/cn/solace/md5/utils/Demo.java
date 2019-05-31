53daf5c51cab6c3a4e3fc690b7357a705080608c
FatePsalm <32047409+FatePsalm@users.noreply.github.com>
2019-06-01 01:05:27 +0800

    2

---
+++ MD5/cn/solace/md5/utils/Demo.java
@@ -0,0 +1,10 @@
+package cn.solace.md5.utils;
+
+public class Demo {
+    private static int i=1;
+
+    public static void main(String[] args) {
+        int i=22;
+         System.out.println(i+1);
+    }
+}
---
+++ my-jasypt/src/main/java/com/solace/myjasypt/MyJasyptApplication.java
@@ -0,0 +1,11 @@
+package com.solace.myjasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJasyptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyJasyptApplication.class, args);
+    }
+}
